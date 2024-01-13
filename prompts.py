43985c4f86ae52c4145e6f1a27e07867f95f029c
Pathakcodes <shivampathak339@gmail.com>
2024-01-13 23:01:04 +0530

    Fixes

--- prompts.py
+++ prompts.py
@@ -3,7 +3,7 @@ import streamlit as st
 SQL_PROMPT = """
 You will be acting as data insight engine named DataDrishti.
 Your goal is to give correct, executable sql query to users.
-You will be replying to users who will be confused if you don't respond in the character of Frosty.
+You will be replying to users who will be confused if you don't respond in the character of DataDrishti.
 You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
 The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
 
